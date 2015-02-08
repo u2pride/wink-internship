52a121854239d4af22ba053d04537d52959984ae
Alex Ryan <thomas.alexander.ryan@gmail.com>
2015-02-07 23:45:24 -0500

    Refactor - renamed ViewController.

--- WinkInternship/LoginVC.m
+++ WinkInternship/LoginVC.m
@@ -7,7 +7,7 @@
 //
 
 
-#import "ViewController.h"
+#import "LoginVC.h"
 #import <CoreImage/CoreImage.h>
 
 static NSString * const BaseAPIString = @"https://winkapi.quirky.com/";
@@ -17,21 +17,21 @@ static NSString * const kUsername = @"usernamekey";
 static NSString * const kPassword = @"passwordkey";
 static NSString * const kLoggedIn = @"loggedinalready";
 
-@interface ViewController ()
+static NSString * const kClientID = @"2ec4f93efd4390a33f6b8dcb12875377";
+static NSString * const kClientSecret = @"d7d606469be78ac2a3fce4e5419ab4f1";
+
+@interface LoginVC ()
 
 @property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 @property (weak, nonatomic) IBOutlet UIImageView *winkImageView;
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
-@property (weak, nonatomic) IBOutlet UIButton *mockServerButton;
-
 
 - (IBAction)login:(id)sender;
 
-
 @end
 
-@implementation ViewController
+@implementation LoginVC
 
 @synthesize usernameTextField, passwordTextField, winkImageView, loginButton, mockServerButton;
 
@@ -60,88 +60,8 @@ static NSString * const kLoggedIn = @"loggedinalready";
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    
-    /*
-
-    NSString *clientID = nil; //add to here instead of directly in string - same with client secret
-    
-    
-    NSURL *URL = [NSURL URLWithString:@"https://winkapi.quirky.com/users/me/wink_devices"];
-    
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:URL];
-    [request setHTTPMethod:@"GET"];
-    
-    [request setValue:@"Bearer 4430835b6071aa0aa16f489f177d809f" forHTTPHeaderField:@"Authorization"];
-    
-    //[request setHTTPBody:[@"{\n    \"client_id\": \"2ec4f93efd4390a33f6b8dcb12875377\",\n    \"client_secret\": \"d7d606469be78ac2a3fce4e5419ab4f1\",\n    \"username\": \"thomas.alexander.ryan@gmail.com\",\n    \"password\": \"1Wink2\",\n    \"grant_type\": \"password\"\n}" dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    NSURLSession *session = [NSURLSession sharedSession];
-    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
-                                            completionHandler:
-                                  ^(NSData *data, NSURLResponse *response, NSError *error) {
-                                      
-                                      if (error) {
-                                          NSLog(@"Error: %@", error);
-                                          
-                                          return;
-                                      }
-                                      
-                                      if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
-                                          NSLog(@"Response HTTP Status code: %ld\n", (long)[(NSHTTPURLResponse *)response statusCode]);
-                                          NSLog(@"Response HTTP Headers:\n%@\n", [(NSHTTPURLResponse *)response allHeaderFields]);
-                                      }
-                                      
-                                      NSString* body = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-                                      NSLog(@"Response Body:\n%@\n", body);
-                                  }];
-    [task resume];
-    
-    NSArray *availableEffects = [CIFilter filterNamesInCategory:kCICategoryColorEffect];
-    
-    NSLog(@"%@", availableEffects);
-    
-    CIFilter *exampleFilter = [CIFilter filterWithName:@"CISephiaTone"];
-    
-    UIImage *filteredImage = [exampleFilter appl]
-    
-    */
-    
-    
-    
-    
-}
-
-
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-    
-
-    /*
-    
-    self.thisIsImage.image = currentBrandImage;
-    
-    // 2
-    CIImage *beginImage = currentBrandImage.CIImage;
-    // 3
-    CIFilter *filter = [CIFilter filterWithName:@"CISepiaTone" keysAndValues: kCIInputImageKey, beginImage, @"inputIntensity", @0.1, nil];
-    CIImage *outputImage = [filter outputImage];
-    
-    // 4
-    UIImage *newImage = [UIImage imageWithCIImage:outputImage];
-    self.thisIsImage.image = newImage;
-    */
-}
 
 - (IBAction)login:(id)sender {
-    
-    
     
     NSString *username = self.usernameTextField.text;
     NSString *password = self.passwordTextField.text;
@@ -155,6 +75,7 @@ static NSString * const kLoggedIn = @"loggedinalready";
     [userDefaults synchronize];
     
     
+    NSString *httpBodyString = [NSString stringWithFormat:@"{\n    \"client_id\": \"%@\",\n    \"client_secret\": \"%@\",\n    \"username\": \"%@\",\n    \"password\": \"%@\",\n    \"grant_type\": \"password\"\n}", kClientID, kClientSecret, username, password];
     
     NSURL *URL = [NSURL URLWithString:@"https://winkapi.quirky.com/oauth2/token"];
     
@@ -163,7 +84,7 @@ static NSString * const kLoggedIn = @"loggedinalready";
     
     [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
     
-    [request setHTTPBody:[@"{\n    \"client_id\": \"2ec4f93efd4390a33f6b8dcb12875377\",\n    \"client_secret\": \"d7d606469be78ac2a3fce4e5419ab4f1\",\n    \"username\": \"thomas.alexander.ryan@gmail.com\",\n    \"password\": \"1Wink2\",\n    \"grant_type\": \"password\"\n}" dataUsingEncoding:NSUTF8StringEncoding]];
+    [request setHTTPBody:[httpBodyString dataUsingEncoding:NSUTF8StringEncoding]];
     
     NSURLSession *session = [NSURLSession sharedSession];
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request
@@ -220,6 +141,74 @@ static NSString * const kLoggedIn = @"loggedinalready";
     [textField resignFirstResponder];
     return NO;
 }
+
+
+
+
+
+/*
+ 
+ NSString *clientID = nil; //add to here instead of directly in string - same with client secret
+ 
+ 
+ NSURL *URL = [NSURL URLWithString:@"https://winkapi.quirky.com/users/me/wink_devices"];
+ 
+ NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:URL];
+ [request setHTTPMethod:@"GET"];
+ 
+ [request setValue:@"Bearer 4430835b6071aa0aa16f489f177d809f" forHTTPHeaderField:@"Authorization"];
+ 
+ //[request setHTTPBody:[@"{\n    \"client_id\": \"2ec4f93efd4390a33f6b8dcb12875377\",\n    \"client_secret\": \"d7d606469be78ac2a3fce4e5419ab4f1\",\n    \"username\": \"thomas.alexander.ryan@gmail.com\",\n    \"password\": \"1Wink2\",\n    \"grant_type\": \"password\"\n}" dataUsingEncoding:NSUTF8StringEncoding]];
+ 
+ NSURLSession *session = [NSURLSession sharedSession];
+ NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+ completionHandler:
+ ^(NSData *data, NSURLResponse *response, NSError *error) {
+ 
+ if (error) {
+ NSLog(@"Error: %@", error);
+ 
+ return;
+ }
+ 
+ if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+ NSLog(@"Response HTTP Status code: %ld\n", (long)[(NSHTTPURLResponse *)response statusCode]);
+ NSLog(@"Response HTTP Headers:\n%@\n", [(NSHTTPURLResponse *)response allHeaderFields]);
+ }
+ 
+ NSString* body = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+ NSLog(@"Response Body:\n%@\n", body);
+ }];
+ [task resume];
+ 
+ NSArray *availableEffects = [CIFilter filterNamesInCategory:kCICategoryColorEffect];
+ 
+ NSLog(@"%@", availableEffects);
+ 
+ CIFilter *exampleFilter = [CIFilter filterWithName:@"CISephiaTone"];
+ 
+ UIImage *filteredImage = [exampleFilter appl]
+ 
+ */
+
+/*
+ 
+ self.thisIsImage.image = currentBrandImage;
+ 
+ // 2
+ CIImage *beginImage = currentBrandImage.CIImage;
+ // 3
+ CIFilter *filter = [CIFilter filterWithName:@"CISepiaTone" keysAndValues: kCIInputImageKey, beginImage, @"inputIntensity", @0.1, nil];
+ CIImage *outputImage = [filter outputImage];
+ 
+ // 4
+ UIImage *newImage = [UIImage imageWithCIImage:outputImage];
+ self.thisIsImage.image = newImage;
+ */
+
+
+//[request setHTTPBody:[@"{\n    \"client_id\": \"2ec4f93efd4390a33f6b8dcb12875377\",\n    \"client_secret\": \"d7d606469be78ac2a3fce4e5419ab4f1\",\n    \"username\": \"thomas.alexander.ryan@gmail.com\",\n    \"password\": \"1Wink2\",\n    \"grant_type\": \"password\"\n}" dataUsingEncoding:NSUTF8StringEncoding]];
+
 
 
 @end
